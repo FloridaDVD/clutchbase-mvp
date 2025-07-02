ec6dc78cca12210e4f4e1ab55dcc4d2f1f11d8f1
CLUTCHBASE <maaninka@live.se>
2025-07-03 00:01:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,3 @@ export default function Home() {
   return (
     <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '5rem' }}>
       <h1>CLUTCHBASE MVP</h1>
-      <p>Nu funkar det! 🎯</p>
-    </div>
-  );
-}
