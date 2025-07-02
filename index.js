882e8524f981c568b278266df59987c716a5fac6
CLUTCHBASE <maaninka@live.se>
2025-07-03 00:12:09 +0200

    Delete index.js

--- index.js
+++
@@ -1,3 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{fontFamily: 'sans-serif',textAlign: 'center',marginTop: '5rem',backgroundColor: '#0f0f0f',color: '#fff',height: '100vh' }}>
