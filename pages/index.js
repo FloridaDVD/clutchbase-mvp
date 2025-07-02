93e518466b9684f6621729a432b5e81afe53dcc8
CLUTCHBASE <maaninka@live.se>
2025-07-03 00:14:28 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
 export default function Home() {
   return (
-    <div style={{ 
-      fontFamily: 'sans-serif', 
-      textAlign: 'center', 
-      marginTop: '5rem', 
-      backgroundColor: '#0f0f0f', 
+    <div style={{
+      fontFamily: 'sans-serif',
+      textAlign: 'center',
+      marginTop: '5rem',
+      backgroundColor: '#111',
       color: '#fff',
-      height: '100vh' 
+      height: '100vh'
     }}>
-      <h1 style={{ fontSize: '3rem', marginBottom: '1rem' }}>CLUTCHBASE MVP</h1>
+      <h1 style={{ fontSize: '3rem' }}>CLUTCHBASE MVP</h1>
       <p style={{ fontSize: '1.2rem' }}>Nu funkar det! 🎯</p>
     </div>
   );
